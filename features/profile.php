b2511042c370b73e55f1ce2c9e9f212a9677b418
Itamar Junior <itjunior@gmail.com>
2022-04-16 20:41:46 +0100

    fixed profile page to use upload function

--- features/profile.php
+++ features/profile.php
@@ -1,5 +1,6 @@
 <?php
 include "functions_prs.php";
+
 if (isset($_GET['submit'])) {
     echo "<script>
     $(document).ready(function(){
@@ -40,35 +41,24 @@ if (isset($_GET['submit'])) {
     </div>';
 }
 
-if (isset($_POST['upload'])) {
-    echo "<script>
-    $(document).ready(function(){
-        $(\"#alertModal\").modal('show');
-    });
-    </script>";
-
-    echo '<!-- Modal Alert -->
-    <div class="modal" id="alertModal" tabindex="-1" aria-labelledby="alertModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                <center>
-                   ';
-    require_once 'upload.php';
-    userSetData($_SESSION["username"], 'agent_pic', $fileNameNew);
-    echo '
-                </center></div>
-            </div>
-        </div>
-    </div>';
+if ($_FILES['picture']['name'] != null) {
+    require "features/functions_upload.php";
+    if (userGetData($_SESSION["username"],'agent_pic') != null) {
+        unlink('features/uploads/' . userGetData($_SESSION["username"],'agent_pic') . '');
+        $fileIDName = uploadFile($_FILES['picture'], $_SESSION["username"], 'agentPic');
+        userSetData($_SESSION["username"], 'agent_pic', $fileIDName);
+        header("Refresh:0");
+    } else {
+        $fileIDName = uploadFile($_FILES['picture'], $_SESSION["username"], 'agentPic');
+        userSetData($_SESSION["username"], 'agent_pic', $fileIDName);
+        header("Refresh:0");
+    }
 }
+
 ?>
 
 <div class="container-fluid">
-    <h3 class="text-dark mb-4">Profile</h3>
+    <h3 class="text-dark mb-4">Profile <?php echo userGetData($_SESSION["username"],'agent_pic'); ?></h3>
     <div class="row mb-3">
         <div class="col-lg-4 col-xl-4">
             <div class="card mb-3">
@@ -77,9 +67,7 @@ if (isset($_POST['upload'])) {
                     <label class="form-label" style="margin: 0;">Your profile picture</label>
                     <br>
                     <form method="POST" enctype="multipart/form-data">
-                        <input type="hidden" name="id" value="<?php echo userGetData($_SESSION["username"], 'agent_id'); ?>" />
-                        <input type="hidden" name="category" value="userProfile" />
-                        <input class="form-control" type="file" name="file">
+                        <input class="form-control" type="file" name="picture">
                         <div class="mb-3"></div>
                         <button class="btn btn-primary btn-sm" type="submit" name="upload">Upload</button>
                     </form>
@@ -113,8 +101,9 @@ if (isset($_POST['upload'])) {
                 </div>
             </div>
         </div>
-
     </div>
+
+
     <div class="col-lg">
         <div class="row">
             <div class="col-xl-12">
