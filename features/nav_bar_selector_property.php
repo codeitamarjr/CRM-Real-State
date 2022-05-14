348797fd81d146ba7cea8b6b894a9534ffc5ca8d
Itamar Junior <itjunior@gmail.com>
2022-05-14 22:26:38 +0100

    fixed style dropmenu property selector

--- features/nav_bar_selector_property.php
+++ features/nav_bar_selector_property.php
@@ -1,40 +1,46 @@
 <?php
 //If the user selects a property from the list will change the session globaly to the selected property $_SESSION["property_code"]
-if(isset($_POST['select_property'])){
+if (isset($_POST['select_property'])) {
     $_SESSION["property_code"] = $_POST['select_property'];
 }
 ?>
 
 
-    <!-- Property Selector Starts -->
-    
-        <div class="nav-item dropdown no-arrow">
-            <a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#">
-                <span class="d-none d-lg-inline me-2 text-gray-600 small">
-                <form method="POST">
-                    <select class="form-select" name="select_property">
+<!-- Property Selector Starts -->
+
+<div class="nav-item dropdown no-arrow">
+    <a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#">
+        <span class="d-none d-lg-inline me-2 text-gray-600 small">
+            <form method="POST">
+                <select class="form-select" name="select_property">
+                    <optgroup>
                         <option selected><?php echo getPropertyData($_SESSION["property_code"], 'property_name'); ?></option>
+                    </optgroup>
+                    <optgroup>
+                        <option disabled>Manage Another Property</option>
+
                         <?php
                         //List all the properties from an agent
                         require 'config/config.php';
                         $select = "SELECT * FROM property WHERE property_prs_code = '$agent_prs_code'";
                         $result = mysqli_query($link, $select);
                         while ($row = mysqli_fetch_array($result)) {
-                            echo '<option value='.$row['property_code'].'>'.$row['property_name'].'</option>';
+                            echo '<option value=' . $row['property_code'] . '>' . $row['property_name'] . '</option>';
                         }
                         mysqli_close($link);
                         ?>
-                    </select>
-                </form>
-            </span>
-            </a>
-        </div>
-    
-    <!-- Property Selector Ends -->
+                    </optgroup>
+                </select>
+            </form>
+        </span>
+    </a>
+</div>
 
-    <script>
-        //auto send form using Jquery
-        $("form").on('change', function() {
-            $("form").trigger('submit');
-        });
-    </script>
+<!-- Property Selector Ends -->
+
+<script>
+    //auto send form using Jquery
+    $("form").on('change', function() {
+        $("form").trigger('submit');
+    });
+</script>
