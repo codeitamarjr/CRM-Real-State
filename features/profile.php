f14cd66d404bfc2e42c02951283d2be754b93539
Itamar Junior <itjunior@gmail.com>
2022-06-25 14:33:34 +0100

    added password update option

--- features/profile.php
+++ features/profile.php
@@ -1,14 +1,61 @@
 <?php
 include "functions_prs.php";
 
-if (isset($_GET['submit'])) {
+// Define variables and initialize with empty values
+$new_password = $confirm_password = "";
+$new_password_err = $confirm_password_err = "";
+
+if (isset($_POST['submit'])) {
+
+    if (!empty(trim($_POST["new_password"])) || !empty(trim($_POST["confirm_password"]))) {
+        // Validate new password
+        if (empty(trim($_POST["new_password"]))) {
+            $new_password_err = "Please enter the new password.";
+        } elseif (strlen(trim($_POST["new_password"])) < 6) {
+            $new_password_err = "Password must have atleast 6 characters.";
+        } else {
+            $new_password = trim($_POST["new_password"]);
+        }
+
+        // Validate confirm password
+        if (empty(trim($_POST["confirm_password"]))) {
+            $confirm_password_err = "Please confirm the password.";
+        } else {
+            $confirm_password = trim($_POST["confirm_password"]);
+            if (empty($new_password_err) && ($new_password != $confirm_password)) {
+                $confirm_password_err = "Password did not match.";
+            }
+        }
+
+        // Check input errors before updating the database
+        if (empty($new_password_err) && empty($confirm_password_err)) {
+            // Set parameters
+            $param_password = password_hash($new_password, PASSWORD_DEFAULT);
+            // Set parameters in the query
+            echo userSetData($_SESSION["username"], 'agent_password', $param_password);
+            echo '<meta http-equiv="refresh" content="0;url=logout.php">';
+        }
+    }
+    
+
+    function setName()
+    {
+        if (isset($_POST['name']) && $_POST['name'] != userGetData($_SESSION["username"], 'agent_name')) {
+            userSetData($_SESSION["username"], 'agent_name', $_POST['name']);
+            echo '<h1><span style="color: #339966;"><center>Your name was updated with success!</center></span></h1>';
+        }
+    };
+
+     
+
     echo "<script>
     $(document).ready(function(){
         $(\"#alertModal\").modal('show');
     });
     </script>";
 
-    echo '<!-- Modal Alert -->
+    echo
+    '<!-- Modal Alert -->
     <div class="modal" id="alertModal" tabindex="-1" aria-labelledby="alertModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
@@ -18,10 +65,7 @@ if (isset($_GET['submit'])) {
                 <div class="modal-body">
                 <center>
                    ';
-    if (isset($_GET['name']) && $_GET['name'] != userGetData($_SESSION["username"], 'agent_name')) {
-        userSetData($_SESSION["username"], 'agent_name', $_GET['name']);
-        echo '<h1><span style="color: #339966;"><center>Your name was updated with success!</center></span></h1>';
-    }
+    setName();
     echo '
                 </center></div>
             </div>
@@ -31,13 +75,13 @@ if (isset($_GET['submit'])) {
 
 if ($_FILES['picture']['name'] != null) {
     require "features/functions_upload.php";
-    if (userGetData($_SESSION["username"],'agent_pic') != null) {
-        unlink('features/uploads/' . userGetData($_SESSION["username"],'agent_pic') . '');
-        $fileIDName = uploadFile($_FILES['picture'], userGetData($_SESSION["username"],'agent_id'), 'agentPic');
+    if (userGetData($_SESSION["username"], 'agent_pic') != null) {
+        unlink('features/uploads/' . userGetData($_SESSION["username"], 'agent_pic') . '');
+        $fileIDName = uploadFile($_FILES['picture'], userGetData($_SESSION["username"], 'agent_id'), 'agentPic');
         userSetData($_SESSION["username"], 'agent_pic', $fileIDName);
         header("Refresh:0");
     } else {
-        $fileIDName = uploadFile($_FILES['picture'], userGetData($_SESSION["username"],'agent_id'), 'agentPic');
+        $fileIDName = uploadFile($_FILES['picture'], userGetData($_SESSION["username"], 'agent_id'), 'agentPic');
         userSetData($_SESSION["username"], 'agent_pic', $fileIDName);
         header("Refresh:0");
     }
@@ -70,16 +114,27 @@ if ($_FILES['picture']['name'] != null) {
                             <p class="text-primary m-0 fw-bold">Agent Data</p>
                         </div>
                         <div class="card-body">
-                            <form method="GET">
+                            <form method="POST">
                                 <input type="hidden" name="access" value="profile">
                                 <div class="row">
                                     <div class="col">
-                                        <div class="mb-3"><label class="form-label" for="first_name"><strong>Full
+                                        <div class="mb-3"><label class="form-label" for="first_name">
+                                                <strong>Full
                                                     Name</strong></label>
                                             <input class="form-control" type="text" value="<?php echo userGetData($_SESSION["username"], 'agent_name'); ?>" name="name">
                                         </div>
                                     </div>
                                 </div>
+                                <div class="form-group">
+                                    <label>New Password</label>
+                                    <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>">
+                                    <span class="invalid-feedback"><?php echo $new_password_err; ?></span>
+                                    <label>Confirm Password</label>
+                                    <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>">
+                                    <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+                                </div>
+
+                                <br>
                                 <div class="mb-3">
                                     <button class="btn btn-primary btn-sm" type="submit" name="submit">Update Details</button>
                                 </div>
@@ -91,5 +146,5 @@ if ($_FILES['picture']['name'] != null) {
         </div>
     </div>
 
-    
+
 </div>
