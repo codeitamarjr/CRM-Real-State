83468cb67876b47bf25fdd375a66b75bd6a01340
Itamar Junior <itjunior@gmail.com>
2022-06-28 21:48:01 +0100

    Added menu name into title of the page w property.

--- dashboard.php
+++ dashboard.php
@@ -47,7 +47,7 @@ if(!$_GET['message_id']){
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Dashboard - Real Enquiries</title>
+    <title><?php if(isset($_GET['access'])) echo (ucfirst($_GET['access'])); else echo 'Dashboard'; ?> | <?php echo getPropertyData($_SESSION["property_code"], 'property_name'); ?> - Real Enquiries</title>
     <meta name="description" content="Customer Relationship Management for Real State Agents">
     <link rel="icon" type="image/x-icon" href="assets/img/fav_logo_size_invert.jpg">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css?h=093230e10e41709a7a3d6ba7f3b3b116">
